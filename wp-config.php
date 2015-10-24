2d8c77e77321695ea64be4176bcd9363cbaff2eb
Marzieh Morovatpasand <mmorova@thoughtworks.com>
2015-10-24 18:10:15 +1100

    Changed salts etc.

--- wp-config.php
+++ wp-config.php
@@ -53,14 +53,15 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+
+define('AUTH_KEY',         'MX>1[GB,*<1k&+lkqO>K:nl&}yTfspD lw;E=/iF1^ohIm](Rn4U_oqr42<PAZMV');
+define('SECURE_AUTH_KEY',  'NS:11au?0+e8N@pt=s=ly}f+Q/kZ$6~tmdgJQXw J#sPue/*c[sn~ Wv|;MbQ<LU');
+define('LOGGED_IN_KEY',    '-pIqFYN/UwQc@r`RNZPU3TB+eC(.3=5-^Q]#?A~s~.=C]{3iWDap0&-i B8X-22L');
+define('NONCE_KEY',        '!|B8uD7aeP[DO+l{3y}&2W*|8E-&=CZ:4##,5cpMy4(v|-g)i|L`x=4G~ULHm87I');
+define('AUTH_SALT',        '7sO<b/h+>yf%]t(YH08>K|Exn&g|c3Z/~G925}-}h-r4m]S3yLy8Or<NiwqKwH8(');
+define('SECURE_AUTH_SALT', 'BJgq:Aq6>tuzC-}#R|>X&)a-a*^[+=@w}+|6?*+tiC{6ia{63u?ZT>?Uuv?HhTi4');
+define('LOGGED_IN_SALT',   '+-LdrB8RyE?#-c|:)ag8+R1bC@Hj= T?/h2=t!?LRIc|$E.PQo(b)$ tMJYdq$7R');
+define('NONCE_SALT',       '48!QeL+g~]^wadFLsvBEMAL:y:BrZ2v @crxFuO7v2.=Tg74g:A .3bTc_|s^R?)');
 
 /**#@-*/
 
